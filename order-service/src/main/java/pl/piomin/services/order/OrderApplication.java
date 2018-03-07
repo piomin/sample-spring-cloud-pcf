fd0c599684c9e6f534fc5e33531ffd9ddebf42f2
piotr.minkowski <piotr.minkowski@gmail.com>
2018-03-07 16:54:14 +0100

    initial commit

---
+++ order-service/src/main/java/pl/piomin/services/order/OrderApplication.java
@@ -0,0 +1,19 @@
+package pl.piomin.services.order;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+public class OrderApplication {
+	
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(OrderApplication.class).web(true).run(args);
+	}
+	
+}
