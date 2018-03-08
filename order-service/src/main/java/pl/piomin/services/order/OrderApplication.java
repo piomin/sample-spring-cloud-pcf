d1e607babbedb1740e24c0c93fc5208a3fd0afa4
Piotr Mińkowski <piotr_minkowski@wp.pl>
2018-03-08 23:48:47 +0100

    fixes

--- order-service/src/main/java/pl/piomin/services/order/OrderApplication.java
+++ order-service/src/main/java/pl/piomin/services/order/OrderApplication.java
@@ -2,14 +2,12 @@ package pl.piomin.services.order;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableCircuitBreaker
 public class OrderApplication {
 	
 	public static void main(String[] args) {
